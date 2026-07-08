5bd0e84a3cdaf5184533328ffa7293377f34343b
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-08 21:50:27 +0200

    v5.0

--- src/app/ui/install.sh
+++ src/app/ui/install.sh
@@ -5,5 +5,18 @@ cd $SCRIPT_DIR
 . $HOME/compute/shared_compute.sh
 install_ngnix
 
-sudo sed -i '/^[[:space:]]*location \/ {$/s/^/# /; /^[[:space:]]*}$/s/^/# /' /etc/nginx/nginx.conf
+# Comment the location / {} in /etc/nginx/nginx.conf
+sudo sed -i '/location \/ {/,/}/c\
+    location / {\
+        proxy_http_version 1.1;\
+        proxy_pass http://localhost:3000/;\
+    }' /etc/nginx/nginx.conf
+
+sudo sed -i '/location \/ {/,/}/c\
+    location / {\
+        proxy_http_version 1.1;\
+        proxy_pass http://localhost:3000/;\
+    }' /etc/nginx/conf.d/nginx_tls.conf
+
+# Restart
 sudo systemctl restart nginx
