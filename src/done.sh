822214a4b3eaab89a990140c15162f7ae6af4f87
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-16 08:44:23 +0200

    v5.0

--- src/done.sh
+++ src/done.sh
@@ -13,6 +13,14 @@ echo "Build done"
 if [ "$UI_URL" != "" ]; then
     echo "URLs" > $FILE_DONE
     append_done "- LangFuse: $UI_URL:3000/"     
+    append_done "  User = $TF_VAR_langfuse_init_user_email"
+    append_done "  Password = $TF_VAR_langfuse_init_user_password"
+    append_done "- Runtime"
+    append_done "  LANGFUSE_PUBLIC_KEY: $TF_VAR_langfuse_init_project_public_key"
+    append_done "  LANGFUSE_SECRET_KEY: $TF_VAR_langfuse_init_project_secret_key"
+    append_done "  LANGFUSE_BASE_URL: http://$BASTION_IP:3000"
+    append_done ""
+    append_done "In this sample the IP is public and the IP range to access it is limited to your Laptop IP"
 elif [ ! -f $FILE_DONE ]; then
     echo "-" > $FILE_DONE  
 fi
--- terraform.tfvars
+++ terraform.tfvars
@@ -11,3 +11,11 @@ public_ip_filter="__TO_FILL__"
 # Compartment
 compartment_ocid="__TO_FILL__"
 
+# LangFuse Init
+# See src/app/langfuse/start.sh
+#     https://langfuse.com/self-hosting/administration/headless-initialization
+
+langfuse_init_user_email="admin@example.com"
+langfuse_init_user_password="changeit"
+langfuse_init_project_public_key="pk-lf-change-it"
+langfuse_init_project_secret_key="sk-lf-change-it"
