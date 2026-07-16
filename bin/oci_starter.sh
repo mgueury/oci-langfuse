7f52e0bf193cf4b127f3712ed2d57f6e4609763f
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-16 19:00:42 +0200

    v5.0

--- bin/oci_starter.sh
+++ bin/oci_starter.sh
@@ -207,8 +207,8 @@ elif [ "$ARG1" == "env" ]; then
 elif [ "$ARG1" == "upgrade" ]; then
     $BIN_DIR/upgrade.sh 
 elif [ "$ARG1" == "network" ]; then
-    . ./starter.sh env -silent
-    echo "test"
+    . ./starter.sh env
+    title "Network OCID"
     get_id_from_tfstate "vcn_ocid" "starter_vcn"
     get_id_from_tfstate "web_subnet_ocid" "starter_web_subnet" 
     get_id_from_tfstate "app_subnet_ocid" "starter_app_subnet" 
