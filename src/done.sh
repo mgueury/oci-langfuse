1ee2faef3158480fadfc878cba391d9cc55facc5
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-19 12:26:11 +0200

    v5.0

--- src/done.sh
+++ src/done.sh
@@ -12,14 +12,15 @@ echo "Build done"
 # Do not show the Done URLs if after_build.sh exists 
 if [ "$UI_URL" != "" ]; then
     echo "URLs" > $FILE_DONE
-    append_done "- LangFuse: $UI_URL:3000/"     
+    append_done "LangFuse: $UI_URL:3000/"     
     append_done "  User = $TF_VAR_langfuse_init_user_email"
     append_done "  Password = $TF_VAR_langfuse_init_user_password"
-    append_done "- Runtime"
-    append_done "  LANGFUSE_PUBLIC_KEY: $TF_VAR_langfuse_init_project_public_key"
-    append_done "  LANGFUSE_SECRET_KEY: $TF_VAR_langfuse_init_project_secret_key"
-    append_done "  LANGFUSE_BASE_URL: public:  http://$BASTION_IP:3000"
-    append_done "                     private: http://$COMPUTE_PRIVATE_IP:3000"    
+    append_done "Runtime"
+    append_done "  export LANGFUSE_PUBLIC_KEY=\"$TF_VAR_langfuse_init_project_public_key\""
+    append_done "  export LANGFUSE_SECRET_KEY=\"$TF_VAR_langfuse_init_project_secret_key\""
+    append_done "  public:  export LANGFUSE_BASE_URL=\"http://$BASTION_IP:3000\""
+    append_done "  private: export LANGFUSE_BASE_URL=\"http://$COMPUTE_PRIVATE_IP:3000\""    
+
     append_done ""
     append_done "In this sample the IP is public and the IP range to access it is limited to your Laptop IP"
 elif [ ! -f $FILE_DONE ]; then
